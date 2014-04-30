863a414e228deaa9f19fc40205e12dd3a31ae43e
Prashanth Kumar <pacificprince@gmail.com>
2014-04-30 11:52:20 -0700

    Some more comments

--- cachematrix.R
+++ cachematrix.R
@@ -10,8 +10,9 @@ makeCacheMatrix <- function(mat = matrix(nrow=0, ncol=0)){
   
   get <- function() mat  # Function to return matrix
   
-  setinv <- function(v) inv <<- v  # Function to cache and store value of calculated inverse
-                                   # This cache value will be used once calculated (without having
+  setinv <- function(v) inv <<- v  # "<<-" is used here to assign a value to an object in a different environment
+                                   # Function to cache and store value of calculated inverse
+                                   # This "cache" value will be used once calculated (without having
                                    # to re-calculate this value every time)
   
   getinv <- function() inv         # Function to return inverse   
@@ -19,10 +20,12 @@ makeCacheMatrix <- function(mat = matrix(nrow=0, ncol=0)){
   return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
-# Function cacheSolve calculates inverse of matrix, accesses cached value if inverse is already calculated
+# Function cacheSolve calculates inverse of the special matrix returned by makeCacheMatrix, 
+# accesses cached value if inverse is already calculated
 cacheSolve <- function(m, ...){
   inv <- m$getinv()
   
+  # If inv already exists, then get its value from cache
   if(!is.null(inv)){ 
     message("getting cached data")
     return(inv) 
